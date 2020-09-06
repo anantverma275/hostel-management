56755c908e059342f261eeda354bd99bb4f281a1
Shikhar Sharma <shikhar21121999@gmail.com>
2020-09-06 09:39:40 +0530

    updating
    made some changes to in try to remove errors but couldnt
    added html and css files to template folder
    connected urls and views to new html files

--- hostelback/app/models.py
+++ hostelback/app/models.py
@@ -2,14 +2,14 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 from django.core.validators import RegexValidator
-phn_validator = RegexValidator(r"^[0-9]{10}$", "Phone number must be of 10 digits only.")
+#phn_validator = RegexValidator(r"^[0-9]{10}$", "Phone number must be of 10 digits only consisting of number form 0-9.")
 
 # Create your models here.
 
 
 class Student(AbstractUser):
     name = models.CharField(max_length = 30, blank = True)
-    ph_no = models.IntegerField(validators=phn_validator)
+    ph_no = models.IntegerField(max_length=10, blank=False)
     roll_no = models.CharField(max_length = 10, unique = True)
     address = models.TextField()
     room_no = models.IntegerField() 
@@ -24,8 +24,9 @@ class Leave(models.Model):
     reason = models.TextField()
     status_accepted = models.BooleanField(default = False)
 
-    def __str__(self):
-	    return "ID Number: " + self.applicant.roll_no +" | Status: "+ self.status_accepted
+    #def __str__(self):
+    
+	#    return "ID Number: " + self.applicant.roll_no +" | Status: "+ self.status_accepted
 
 class Complaint(models.Model):
     description = models.TextField()
--- hostelback/app/urls.py
+++ hostelback/app/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.signin, name = "signin"),
     path('verify', views.verify, name="verify"),
-    path('dashboard', views.dashboard, name="dashboard")
+    path('dashboard', views.dashboard, name="dashboard"),
+    path('leave', views.leave, name="leave"),
+    path('complaint', views.complaint, name="complaint"),
+    path('admin/register', views.register, name="register")
 ]
